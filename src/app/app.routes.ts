c3c1e06d25f50c17cfa528be7b3402452d413958
Natalia Benko <characteronme@gmail.com>
2024-09-05 16:04:02 +0300

    feat: setup project;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home-page/home-page.component'),
+  },
+  {
+    path: 'country',
+    loadComponent: () => import('./country-page/country-page.component'),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/home-page/ui/country-search/country-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-search.component.html',
+  styleUrl: './country-search.component.scss',
+})
+export class CountrySearchComponent {}
